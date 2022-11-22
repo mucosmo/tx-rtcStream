21288f7435889614d9ee7dd635d9ebf071eb89c4
huanyu.yang <mfuture@qq.com>
2022-11-22 09:47:13 +0000

    fix liveUrl produce

--- server/lib/stream_pipeline/pull.js
+++ server/lib/stream_pipeline/pull.js
@@ -4,24 +4,27 @@
 
 const GStreamer = require('../gstreamer/command-playurl')
 
+const fs = require('fs')
+
 /**
  *  把房间的音视频流转化成直播地址
  */
-module.exports.liveStreamUrl =  (roomId, peerId) => {
+module.exports.liveStreamUrl = (roomId, peerId) => {
     const recordInfo = global.streamInfo[roomId][peerId]
-    const consumers = global.streamInfo[roomId][peerId]["consumers"]
+    const filePath = `${recordInfo.fileName}/mediasoup_live.m3u8`
+    if (!fs.existsSync(`/opt/www/tx-rtcStream/files/${filePath}`)) {
+        const consumers = global.streamInfo[roomId][peerId]["consumers"]
+        global.peer.process = new GStreamer(recordInfo);
+        setTimeout(async () => {
+            for (const [id, consumer] of consumers) {
+                // Sometimes the consumer gets resumed before the GStreamer process has fully started
+                // so wait a couple of seconds
+                await consumer.resume();
+                await consumer.requestKeyFrame();
+            }
+        }, 1000);
+    }
 
-    global.peer.process = new GStreamer(recordInfo);
 
-
-    setTimeout(async () => {
-        for (const [id, consumer] of consumers) {
-            // Sometimes the consumer gets resumed before the GStreamer process has fully started
-            // so wait a couple of seconds
-            await consumer.resume();
-            await consumer.requestKeyFrame();
-        }
-    }, 1000);
-
-    return `http://hz-test.ikandy.cn:60125/files/${recordInfo.fileName}/mediasoup_live.m3u8`
+    return `http://hz-test.ikandy.cn:60125/files/${filePath}`
 }
