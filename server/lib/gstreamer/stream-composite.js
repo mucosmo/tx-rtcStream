42de511caa7b8d956e3393a6afab424b628a50da
huanyu.yang <mfuture@qq.com>
2022-11-30 05:13:38 +0000

    add audio to composite

--- server/lib/gstreamer/stream-composite.js
+++ server/lib/gstreamer/stream-composite.js
@@ -4,7 +4,7 @@ const child_process = require('child_process');
 const { EventEmitter } = require('events');
 const shell = require('shelljs');
 
-const fs =require('fs')
+const fs = require('fs')
 
 const { getCodecInfoFromRtpParameters } = require('./utils');
 
@@ -54,7 +54,7 @@ module.exports = class GStreamer {
       console.log('gstreamer::process::stderr::data [data:%o]', data)
     );
 
-    this._process.stdout.on('data', data =>{
+    this._process.stdout.on('data', data => {
       // console.log('gstreamer::process::stdout::data [data:%o]', data.length)
     }
     );
@@ -142,18 +142,28 @@ module.exports = class GStreamer {
     const fileName = this._rtpParameters.fileName;
     const RECORD_FILE_LOCATION_PATH = `/opt/www/tx-rtcStream/files/composite`;
 
-    if(!fs.existsSync(RECORD_FILE_LOCATION_PATH)){
+    if (!fs.existsSync(RECORD_FILE_LOCATION_PATH)) {
       fs.mkdirSync(RECORD_FILE_LOCATION_PATH)
     }
 
-    const video2 = '/opt/www/tx-rtcStream/files/resources/video2.mp4';
-    const rtmp='rtmp:\/\/175.178.31.221:51013\/live\/m23902200782651393';
     const dh = '/opt/www/tx-rtcStream/files/resources/dh.mp4';
+    const video1 = '/opt/www/tx-rtcStream/files/resources/filevideo.mp4';
+    const video2 = '/opt/www/tx-rtcStream/files/resources/video2.mp4';
+    const png = '/opt/www/tx-rtcStream/files/resources/fileimage.png';
+    const gif = '/opt/www/tx-rtcStream/files/resources/gif.gif';
+    const mask = '/opt/www/tx-rtcStream/files/resources/mask.png';
+    const svg = '/opt/www/tx-rtcStream/files/resources/svg.svg';
+    const rtmp = 'rtmp://175.178.31.221:51013/live/m24072639832129537';
+    const m3u8 = 'http://hz-test.ikandy.cn:60125/files/1669358475054g2l5bihp6e/mediasoup_live.m3u8';
+    const subtitles = '/opt/www/tx-rtcStream/files/resources/subtitles.srt';
+    const font = '/usr/share/fonts/chinese/SIMKAI.TTF';
+    const drawtext = '你好啊';
+    const drawtextfile = '/opt/www/tx-rtcStream/files/resources/drawtext.txt';
 
     return [
       'matroskamux name=mux',
       `! fdsink`,
-      `| ffmpeg -y -i -  -i ${rtmp} -filter_complex "[1:v]scale=200:-1,chromakey=0x00ff00:0.3:0.05[ov1];[0][ov1]overlay=0:H*0.4" -c:v libx264  -preset slow -crf 25 ${RECORD_FILE_LOCATION_PATH}/${Date.now()}.mp4`
+      `| ffmpeg -y -i -  -i ${png} -i ${mask} -i ${video2} -i ${gif} -i ${rtmp}  -filter_complex "[1]crop=100:50:200:200[cropped1];[2]alphaextract[amask];[amask]scale=150:150[vmask];[3:v]scale=150:150[cropped3];[cropped3][vmask]alphamerge[avatar];[0][cropped1]overlay=W-w-10:10[ov1];[ov1][avatar]overlay=10:10[ov2];[4:v]scale=50:50[gif];[ov2][gif]overlay=W-w-10:H/2[ov3];[5]scale=150:-1,chromakey=0x00ff00:0.3:0.05[ov4];[ov3][ov4]overlay=-20:H*0.6[ov5];[ov5]subtitles=${subtitles}[final];[final]drawtext=textfile=${drawtextfile}:fontfile=${font}:x=(w-text_w)/2:y=h-80*t:fontcolor=white:fontsize=40:shadowx=2:shadowy=2;[0:a][5:a]amix" -max_muxing_queue_size 1024  -c:v libx264  -preset slow -crf 25 ${RECORD_FILE_LOCATION_PATH}/${Date.now()}.mp4`
     ];
   }
 }
