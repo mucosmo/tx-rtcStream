529672dd91ba0675a4c716be54431eb881945a09
huanyu.yang <mfuture@qq.com>
2022-12-07 16:58:50 +0800

    update build.sh

--- server/clan/filter/build.sh
+++ server/clan/filter/build.sh
@@ -1,3 +1,6 @@
-rm -rf filtering_video.o
+rm -rf filtering_video.o transcoding.o
+
 make
-gcc -fPIC -shared -rdynamic  filtering_video.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o filter.so
+gcc -fPIC -shared -rdynamic  filtering_video.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o filtering_video.so
+
+gcc -fPIC -shared -rdynamic  transcoding.o -L/opt/program/ffmpeg/lib -lavcodec -lavformat -lavfilter -lavutil -lavdevice -o transcoding.so
