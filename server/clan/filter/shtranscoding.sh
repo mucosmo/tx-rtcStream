6eff1717d16c12ed2fce7c7511c5d59b32d0f277
huanyu.yang <mfuture@qq.com>
2022-12-09 09:42:13 +0800

    transcoding command and input

---
+++ server/clan/filter/shtranscoding.sh
@@ -0,0 +1,10 @@
+
+timestamp=$(date +%s)
+outputvideo=/opt/application/tx-rtcStream/files/composites/c-${timestamp}.mp4
+
+# # RTMP 推流
+# ./transcoding /opt/application/tx-rtcStream/files/resources/screen5s.mp4 rtmp://121.5.133.154:1935/myapp/12345
+
+
+# RTMP 生成文件
+./transcoding /opt/application/tx-rtcStream/files/resources/screen5s.mp4 ${outputvideo}
